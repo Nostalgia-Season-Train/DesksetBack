84a3504158beebe0068e5d1ba86e8fe791cf9420
旧日丨四季列车 <Nostalgia-Season-Train@outlook.com>
2024-12-04 00:22:17 +0800

    标准输入输出：添加国际化

--- src/deskset/core/standard.py
+++ src/deskset/core/standard.py
@@ -1,17 +1,20 @@
 from dataclasses import dataclass
 from typing import Any
 
+from deskset.core.locale import _t
+
+
 @dataclass
 class DesksetSuccess():
     code:    int = 0
-    message: str = 'Success'
+    message: str = _t('Success')
     data:    Any = None
 
 class DesksetError(Exception):
     def __init__(
             self,
             code:    int = 1,
-            message: str = 'Failure',
+            message: str = _t('Failure'),
             data:    Any = None
         ):
         self.code    = code
@@ -22,5 +25,5 @@ class DesksetError(Exception):
 class DesksetReturn():
     success: bool = False
     code:    int  = -1
-    message: str  = 'Not Sure Success Or Failure'
+    message: str  = _t('Not Sure Success Or Failure')
     data:    Any  = None
