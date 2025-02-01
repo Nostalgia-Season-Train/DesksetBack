bb08c41e117faf67232524493b69008c52f1c9f5
旧日丨四季列车 <Nostalgia-Season-Train@outlook.com>
2025-02-01 13:06:16 +0800

    新增功能：通过 vscode 打开文件夹 代码优化：添加输入验证，路径是否为文件夹、可执行文件

--- src/deskset/router/quick.py
+++ src/deskset/router/quick.py
@@ -1,21 +1,55 @@
-from fastapi import APIRouter
-from pydantic import BaseModel
+# === 输入验证 ===
+from pydantic import BaseModel, field_validator
 
-import deskset.feature.quick as quick
+import os
+from deskset.core.standard import DesksetError
 
-router_quick = APIRouter(prefix='/v0/quick', tags=['快速启动'])
-
-class QuickOpenPath(BaseModel):
+class RequestFolder(BaseModel):
     path: str
 
-class QuickOpenURL(BaseModel):
+    @field_validator('path')
+    @classmethod
+    def check_folder(cls, v: str) -> str:
+        if not os.path.isdir(v):
+            raise DesksetError(message=f'错误！{v} 不是文件夹！')
+        return v
+
+class RequestApp(BaseModel):
+    path: str
+
+    @field_validator('path')
+    @classmethod
+    def check_app(cls, v: str) -> str:
+        # Linux 下可执行文件没有后缀，需要其他检查手段
+        if os.path.isfile(v) != True or os.path.splitext(v)[1] != '.exe':
+            raise DesksetError(message=f'错误！{v} 不是应用！')
+        return v
+
+class RequestWeb(BaseModel):
     url: str
 
 
-@router_quick.post('/open-app-by-path')
-def open_app_by_path(req: QuickOpenPath):
-    quick.open_app_by_path(req.path)
+# === 路由 ===
+from fastapi import APIRouter
 
-@router_quick.post('/open-web-by-url')
-def open_web_by_url(req: QuickOpenURL):
+from deskset.presenter.format import format_return
+
+import deskset.feature.quick as quick
+import deskset.feature.app.vscode as app_vscode
+
+router_quick = APIRouter(prefix='/v0/quick', tags=['快速启动'])
+
+@router_quick.post('/open-app-through-path')
+def open_app_through_path(req: RequestApp):
+    quick.open_app_by_path(req.path)
+    return format_return('成功打开应用：{}'.format(req.path))
+
+@router_quick.post('/open-web-through-url')
+def open_web_through_url(req: RequestWeb):
     quick.open_web_by_url(req.url)
+    return format_return('成功打开网站：{}'.format(req.url))
+
+@router_quick.post('open-folder-by-vscode')
+def open_folder_by_vscode(req: RequestFolder):
+    app_vscode.open_folder_by_vscode(req.path)
+    return format_return('成功通过 vscode 打开文件夹：{}'.format(req.path))
