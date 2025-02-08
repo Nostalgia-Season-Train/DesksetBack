88a9c9d3d4a45cf83145d6054c86c2d244d0d028
旧日丨四季列车 <Nostalgia-Season-Train@outlook.com>
2025-02-09 01:56:52 +0800

    优化配置 deskset.json 读取方式，相应模块 deskset.core.config

--- src/deskset/core/config.py
+++ src/deskset/core/config.py
@@ -1,36 +1,82 @@
-from dynaconf import Dynaconf
+import json
 
-CONFIG_MAIN = './config/deskset-back.json'
+from deskset.core.log import logging
+
+CONFIG_MAIN_PATH = './config/deskset.json'
+CONFIG_MAIN_ENCODE = 'utf-8'
 
 
 class Config(object):
     _instance = None
 
-    def __new__(cls):
+    def __new__(cls) -> None:
         if cls._instance is None:
             cls._instance = object.__new__(cls)
         return cls._instance
 
-    def __init__(self):
+    def __init__(self) -> None:
         if not hasattr(self._instance, '_is_init'):
             self._is_init = True
+            self._init_once()
 
-            self.refresh()
+    def _init_once(self) -> None:
+        # 1、属性设为默认值
+        # 2、读取，检查通过后修改属性
+        # 3、写入，属性覆盖上一步无效配置
+        # 注意！不要添加跟配置无关的公有成员属性，此类依靠自身属性读取 json 配置
 
-    def refresh(self):
-        # 读取设置
-        self._setting = Dynaconf(
-            envvar_prefix  = 'DYNACONF',
-            settings_files = CONFIG_MAIN
-        )
-
-        # 语言：默认 中文
-        # 编码：默认 UTF-8
-        self.language = self._setting.get('language', default='zh-cn')
-        self.encoding = self._setting.get('encoding', default='utf-8')
-
+        # === 默认值 ===
+        # 语言和编码
+        self.language: str = 'zh-cn'
+        self.encoding: str = 'utf-8'
         # 端口
-        self.server_port: int = int(self._setting.get('server-port', default='8000'))
+        self.server_port: int = 8000
+        # 用户和密码
+        self.username: str = 'deskset-username'
+        self.password: str = 'deskset-password'
+
+        # === 读取 ===
+        try:
+            with open(CONFIG_MAIN_PATH, 'r', encoding=CONFIG_MAIN_ENCODE) as file:
+                data: dict = json.load(file)
+
+                for attr_key, attr_value in list(self.__dict__.items()):  # list 创建副本后修改 self 属性
+                    # 不是私有成员属性
+                    if attr_key.startswith('_'):
+                        continue
+
+                    # 配置类型跟默认值一致
+                    config_key = attr_key.replace('_', '-')
+                    config_type = type(attr_value)
+                    if type(data.get(config_key)) != config_type:
+                        continue
+
+                    # 修改属性
+                    value = data.get(config_key)
+                    if   config_type == type(10000):
+                        setattr(self, attr_key, value)
+                    elif config_type == type('str') and value != '':
+                        setattr(self, attr_key, value)
+                    else:
+                        pass
+        except FileNotFoundError:
+            logging.warning(f'{CONFIG_MAIN_PATH} not found')
+            pass
+        except json.JSONDecodeError:
+            logging.warning(f'{CONFIG_MAIN_PATH} decode failed')
+            pass
+
+        # === 写入 ===
+        with open(CONFIG_MAIN_PATH, 'w', encoding=CONFIG_MAIN_ENCODE) as file:
+            data: dict = {
+                key.replace('_', '-'): value for key, value in self.__dict__.items() if not key.startswith('_')
+            }
+            json.dump(data, file, ensure_ascii=False, indent=4)
 
 
 config = Config()
+
+
+if __name__ == '__main__':
+    for attr_key, attr_value in config.__dict__.items():
+        print(attr_key, attr_value)
