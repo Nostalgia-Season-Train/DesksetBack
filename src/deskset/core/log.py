41a59c8a55866e11c1e9c940ccefa2412cc4a8f1
旧日丨四季列车 <Nostalgia-Season-Train@outlook.com>
2025-02-09 21:49:18 +0800

    日志：修复时间格式

--- src/deskset/core/log.py
+++ src/deskset/core/log.py
@@ -4,7 +4,7 @@ logging.basicConfig(
     filename='log/latest.log',
     filemode='w+',
     format='[%(asctime)s] [%(levelname)s]: %(message)s',
-    datefmt='%Y-%M-%d %H:%M:%S',
+    datefmt='%Y-%m-%d %H:%M:%S',
     level=logging.INFO,
     encoding='utf-8'
 )
