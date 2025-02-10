11cf6017b9246c57d13820b8176274716a478afc
旧日丨四季列车 <Nostalgia-Season-Train@outlook.com>
2025-02-10 14:52:01 +0800

    日志：FileNotFoundError 也可能由目录不存在引发，跟 w 或 w+ 模式无关

--- src/deskset/core/log.py
+++ src/deskset/core/log.py
@@ -2,7 +2,7 @@ import logging
 
 logging.basicConfig(
     filename='log/latest.log',
-    filemode='w+',
+    filemode='w',  # 注：目录不存在也会抛出 FileNotFoundError 异常
     format='[%(asctime)s] [%(levelname)s]: %(message)s',
     datefmt='%Y-%m-%d %H:%M:%S',
     level=logging.INFO,
