5a88b86fe3506c8647cf54309cd7f34a5ce98728
旧日丨四季列车 <Nostalgia-Season-Train@outlook.com>
2025-02-28 16:16:58 +0800

    构建：优化构建流程 - 注：settings.json 强制 vscode 4 缩进，否则 setup.py 无论是 Tab 还是代码提示都是 2 缩进

--- setup.py
+++ setup.py
@@ -1,21 +1,37 @@
 import shutil, os
 
+DIST = 'dist/Deskset-Back'  # 构建产物的存放路径
+
+
 # ==== 预处理 ====
 
-# 清除上次文件
-shutil.rmtree('./dist/DigitalDeskSetup/site-packages', ignore_errors=True)
-shutil.rmtree('./dist/DigitalDeskSetup/i18n', ignore_errors=True)
+# 清除上次构建和测试产生的文件及文件夹
+def clear(folder):
+    shutil.rmtree(f'./{DIST}/{folder}', ignore_errors=True)
+
+clear('site-packages')
+
+clear('config')
+clear('log')
+
+clear('i18n')
+
+clear('api')
 
 
 # ==== 打包 ====
 
 # 下载依赖
-os.system('pip install -r requirements.txt -t ./dist/DigitalDeskSetup/site-packages')
+os.system(f'pip install -r requirements.txt -t ./{DIST}/site-packages')
 
 # 构建程序
   # 构建失败的解决方案
   # - Error 1104：避免中文路径
-os.system('nuitka --module src/deskset --include-package=deskset --output-dir=dist/DigitalDeskSetup/site-packages --remove-output')
+os.system(f'nuitka --module src/deskset --include-package=deskset --output-dir={DIST}/site-packages --remove-output')
 
-# 复制 i18n 翻译文件
-shutil.copytree('./i18n', './dist/DigitalDeskSetup/i18n')
+# 复制 翻译文件、示例插件
+def copy(folder):
+    shutil.copytree(f'./{folder}', f'./{DIST}/{folder}')
+
+copy('i18n')
+copy('api')
