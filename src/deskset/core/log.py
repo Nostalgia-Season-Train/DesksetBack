bd6ee492356252cc1453a55ef7724311b8436978
旧日丨四季列车 <Nostalgia-Season-Train@outlook.com>
2025-03-02 17:53:32 +0800

    日志：修复运行时，先写入的日志被 logging.exception() 覆写清除的问题

--- src/deskset/core/log.py
+++ src/deskset/core/log.py
@@ -1,8 +1,15 @@
 import logging
 
+with open('log/latest.log', 'w') as file:
+    # 清空上次日志
+    pass
+
 logging.basicConfig(
     filename='log/latest.log',
-    filemode='w',  # 注：目录不存在也会抛出 FileNotFoundError 异常
+    # filemode 注释
+      # 注 1：目录不存在也会抛出 FileNotFoundError 异常
+      # 注 2：用 a 模式写入，w 模式会意外覆盖运行时日志
+    filemode='a',
     format='[%(asctime)s] [%(levelname)s]: %(message)s',
     datefmt='%Y-%m-%d %H:%M:%S',
     level=logging.INFO,
