3058199afddfb456c213dd12f89b2b0614cee6ab
旧日丨四季列车 <Nostalgia-Season-Train@outlook.com>
2025-03-10 20:02:45 +0800

    打包：添加 PyStand 追踪，方便上传到仓库 CI 构建

---
+++ res/PyStand/README.md
@@ -0,0 +1,3 @@
+DesksetBack 运行环境（win32-x64）
+- Python：https://www.python.org
+- PyStand：https://github.com/skywind3000/PyStand
--- setup.py
+++ setup.py
@@ -1,5 +1,6 @@
 import shutil, os
 
+RUNON = 'res/PyStand/PyStand-v1.1.4+Python-v3.9.7.zip'
 DIST = 'dist/DesksetBack'  # 构建产物的存放路径
 
 def clear(folder):
@@ -10,19 +11,16 @@ def copy(folder):
 
 # ==== 预处理 ====
 
-# 创建输出目录
+# 清除上次构建
+shutil.rmtree(f'./{DIST}', ignore_errors=True)
+
+# 创建运行环境
+import zipfile
+
 os.makedirs(f'./{DIST}', exist_ok=True)
 
-# 清除上次构建和测试产生的文件及文件夹
-clear('site-packages')
-clear('lib')
-
-clear('config')
-clear('log')
-
-clear('i18n')
-
-clear('api')
+with zipfile.ZipFile(f'./{RUNON}', 'r') as file:
+    file.extractall(f'./{DIST}')
 
 
 # ==== 打包 ====
@@ -36,6 +34,8 @@ os.system(f'pip install -r requirements.txt -t ./{DIST}/site-packages')
 os.system(f'nuitka --module src/deskset --include-package=deskset --output-dir={DIST}/site-packages --remove-output')
 
 # 编译 C/C++ 代码
+os.makedirs(f'./lib', exist_ok=True)  # 创建 lib 二进制库，否则 gcc 报错
+
 os.system('cd src/package_C/disk_active_time && build.bat')
 
 copy('lib')
