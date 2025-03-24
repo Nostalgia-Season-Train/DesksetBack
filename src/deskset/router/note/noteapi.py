99d7afaf71c0a45b807aebde189be0bb9042b625
旧日丨四季列车 <Nostalgia-Season-Train@outlook.com>
2025-03-25 01:56:30 +0800

    笔记/NoteAPI：禁止 httpx 输出 ERROR 级别以下的日志

--- src/deskset/router/note/noteapi.py
+++ src/deskset/router/note/noteapi.py
@@ -3,3 +3,9 @@ from httpx import AsyncClient
 from deskset.core.config import config
 
 noteapi = AsyncClient(base_url=f'http://{config.noteapi_host}:{config.noteapi_port}')
+
+
+# 禁止 httpx 输出 ERROR 级别以下的日志
+import logging
+
+logging.getLogger('httpx').setLevel(logging.ERROR)
