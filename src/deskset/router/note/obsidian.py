2c8b255025420dcad15aa58231c74fe780428a9e
旧日丨四季列车 <Nostalgia-Season-Train@outlook.com>
2025-03-25 01:17:18 +0800

    笔记/Obsidian - 新增 /read-month/{month} 读取某月中的日记 - 优化命名

--- src/deskset/router/note/obsidian.py
+++ src/deskset/router/note/obsidian.py
@@ -4,7 +4,7 @@ from deskset.core.config import config
 from deskset.router.stand_response import DesksetJSONResponse
 
 from .noteapi import noteapi
-from .validate import DesksetReqDateDay
+from .validate import DesksetReqDateDay, DesksetReqDateMonth
 
 router_obsidian = APIRouter(
     prefix='/v0/obsidian', tags=['Obsidian']
@@ -25,16 +25,17 @@ async def today_tasks():
     diary_tasks = (await noteapi.post(f'/tasks/get-all-tasks', data={'notepath': diary['notepath']})).json()
     return diary_tasks
 
-@router_diary.get('/read/{day}')
-async def read(date: DesksetReqDateDay = Depends()):
-    diary = (await noteapi.get(f'/diary/read/{date.day}')).json()
+# 读取某天日记（日期格式：YYYYMMDD）
+@router_diary.get('/read-day/{day}')
+async def read_day(date: DesksetReqDateDay = Depends()):
+    diary = (await noteapi.get(f'/diary/read-day/{date.day}')).json()
     return diary
 
-@router_diary.get('/read-tasks/{day}')
-async def read_tasks(date: DesksetReqDateDay = Depends()):
-    diary = (await noteapi.get(f'/diary/read/{date.day}')).json()
-    diary_tasks = (await noteapi.post(f'/tasks/get-all-tasks', data={'notepath': diary['notepath']})).json()
-    return diary_tasks
+# 读取某月中的日记（日期格式：YYYYMM）
+@router_diary.get('/read-month/{month}')
+async def read_month(date: DesksetReqDateMonth = Depends()):
+    diarys = (await noteapi.get(f'/diary/read-month/{date.month}')).json()
+    return diarys
 
 
 # ==== 注册 Obsidian 的子路由 ====
