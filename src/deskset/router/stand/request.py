4d8fe129d95c7227cc13213fc9de5355742dd267
旧日丨四季列车 <Nostalgia-Season-Train@outlook.com>
2025-04-02 23:22:37 +0800

    路由/标准化 新增：验证网址请求

--- src/deskset/router/stand/request.py
+++ src/deskset/router/stand/request.py
@@ -70,6 +70,22 @@ class DesksetReqApp(BaseModel):
         return v
 
 
-# ==== 请求网址 ====
+# ==== 验证网址 ====
+from urllib.parse import urlparse
+
 class DesksetReqURL(BaseModel):
     url: str
+
+    @field_validator('url')
+    @classmethod
+    def check_folder(cls, v: str) -> str:
+        parsed_url = urlparse(v)
+        scheme = parsed_url.scheme
+        netloc = parsed_url.netloc
+        # 检查网络协议
+        if scheme != 'http' and scheme != 'https':
+            raise DesksetError(message=f'错误！网络协议 {scheme} 无效！')
+        # 检查网络位置
+        if not netloc:
+            raise DesksetError(message=f'错误！网络位置 {netloc} 无效！')
+        return v
