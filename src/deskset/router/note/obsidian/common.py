d35e11b4f461577e2518d2912bf739e581555e26
旧日丨四季列车 <Nostalgia-Season-Train@outlook.com>
2025-04-22 22:58:55 +0800

    Obsidian/通用 改进：优化 /recent-notes 第一个为现在打开的笔记

--- src/deskset/router/note/obsidian/common.py
+++ src/deskset/router/note/obsidian/common.py
@@ -9,10 +9,19 @@ router_common = APIRouter(prefix='/common')
 async def get_current_note():
     return (await noteapi.get('/obsidian/current-note')).text
 
+# 最近打开的笔记
 @router_common.get('/recent-notes')
 async def get_recent_notes():
     workspace = (await noteapi.get('/obsidian/workspace')).json()
     recent_files = workspace.get('lastOpenFiles', []) if isinstance(workspace, dict) else []
+
+    # lastOpenFiles 第一个不是现在打开的笔记，需要修正
+    # 比如：按顺序打开笔记 1、2、3
+      # recent_files = [1, 2]
+      # current_file = 3
+    current_file = (await noteapi.get('/obsidian/current-note')).text
+    recent_files = [current_file] + [file for file in recent_files if file != current_file]
+
     from pathlib import Path
     return [
         { 'name': Path(file).stem, 'path': file }
