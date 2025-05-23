8cf097d37ee73d47dd083511f5a14571e76a19da
旧日丨四季列车 <Nostalgia-Season-Train@outlook.com>
2025-05-24 03:26:58 +0800

    DesksetBack 项目调整： - 通过 uv 管理依赖 - Python 升级到 3.12.10 版本

--- setup.py
+++ setup.py
@@ -1,6 +1,6 @@
 import shutil, os
 
-RUNON = 'res/PyStand/PyStand-v1.1.4+Python-v3.9.7.zip'
+RUNON = 'res/PyStand/PyStand-v1.1.5+Python-v3.12.10_x86-64.zip'
 DIST = 'dist/DesksetBack'  # 构建产物的存放路径
 
 def clear(folder):
@@ -23,19 +23,21 @@ with zipfile.ZipFile(f'./{RUNON}', 'r') as file:
     file.extractall(f'./{DIST}')
 
 # 替换入口文件
-os.remove(f'./{DIST}/DesksetBack.py')
+# os.remove(f'./{DIST}/DesksetBack.py')  # 仅限 PyStand-v1.1.4+Python-v3.9.7.zip
 shutil.copyfile(src=f'./main-prod.py', dst=f'./{DIST}/DesksetBack.py')
 
 
 # ==== 打包 ====
 
 # 下载依赖
-os.system(f'pip install -r requirements.txt -t ./{DIST}/site-packages')
+# os.system(f'pip install -r requirements.txt -t ./{DIST}/site-packages')
+shutil.copytree('./.venv/Lib/site-packages', f'./{DIST}/site-packages')  # 直接复制 uv 环境
 
 # 构建程序
   # 构建失败的解决方案
   # - Error 1104：避免中文路径
-os.system(f'nuitka --module src/deskset --include-package=deskset --output-dir={DIST}/site-packages --remove-output')
+# os.system(f'nuitka --module src/deskset --include-package=deskset --output-dir={DIST}/site-packages --remove-output')
+shutil.copytree('./src/deskset', f'./{DIST}/site-packages/deskset')  # nuitka 暂不支持 3.12.10...
 
 # 编译 C/C++ 代码
 os.makedirs(f'./lib', exist_ok=True)  # 创建 lib 二进制库，否则 gcc 报错
