b125d7bebe3b5b145f1222bbea726fa95fc8abbc
旧日丨四季列车 <Nostalgia-Season-Train@outlook.com>
2025-07-20 00:21:06 +0800

    入口函数-生产 修复：热重载失效和控制台打印乱码

--- main-prod.py
+++ main-prod.py
@@ -1,17 +1,24 @@
-# === 检查参数 ===
+# === PyStand 环境：避免多进程无限循环创建 ===
 import sys
+import multiprocessing
+
+if not hasattr(sys, 'frozen'):
+    sys.frozen = True
+
+multiprocessing.freeze_support()
+
+
+# === 检查参数 ===
 args = sys.argv
 DEVELOP_ENV = True if '-dev' in args else False
 
 
 # === 运行程序 ===
-from multiprocessing import freeze_support
-
 if __name__ == '__main__':
-    freeze_support()  # 唯一的作用，就是排除由于 freeze_support 没有添加引发的错误
     if DEVELOP_ENV:
         import uvicorn
-        uvicorn.run('deskset.main:app', host='127.0.0.1', port=6527, reload=True)
+        # use_colors=False：cmd.exe 不支持打印颜色，显示乱码
+        uvicorn.run('deskset.main:app', host='127.0.0.1', port=6527, reload=True, use_colors=False)
     else:
         # -dev 启动，虽然经过判断，但是下面的代码仍会执行，导致默认端口 6527 重复占用
           # 暂时不处理
