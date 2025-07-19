ad90b4a69ed087f7b5ff501eb436521442267bc5
旧日丨四季列车 <Nostalgia-Season-Train@outlook.com>
2025-07-19 11:25:50 +0800

    入口函数 改进：去掉生产环境换行打印，影响标准输出流

--- main-prod.py
+++ main-prod.py
@@ -7,8 +7,6 @@ DEVELOP_ENV = True if '-dev' in args else False
 # === 运行程序 ===
 from multiprocessing import freeze_support
 
-print()  # 从 > 下一行开始打印
-
 if __name__ == '__main__':
     freeze_support()  # 唯一的作用，就是排除由于 freeze_support 没有添加引发的错误
     if DEVELOP_ENV:
